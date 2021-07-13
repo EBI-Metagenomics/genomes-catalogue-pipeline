f0fa538b47b35ec0111b27b26d0c70c62ffc28c3
Ekaterina Sakharova <kates@ebi.ac.uk>
2021-07-13 18:09:27 +0100

    add test mash2nwk

--- docker/build.sh
+++ docker/build.sh
@@ -5,29 +5,34 @@ export NAME=genomes-pipeline
 # checkm
 cd  checkm && docker build -t microbiomeinformatics/${NAME}.checkm:v1.1.3 .
 
-# drep [ pushed ]
+# drep
 cd drep && docker build -t microbiomeinformatics/${NAME}.drep:v1 .
 
 # eggnog
-cd eggnong && docker build -t microbiomeinformatics/${NAME}.eggnog:v1 .
+cd eggnong && docker build -t microbiomeinformatics/${NAME}.eggnog-mapper:v1 .
 
-# genome-catalog-update [ pushed ]
+# genome-catalog-update
 cd genomes-catalog-update && docker build -t microbiomeinformatics/${NAME}.genome-catalog-update:v1 .
-
-# mmseqs
-cd  mmseqs && docker build -t microbiomeinformatics/${NAME}-mmseqs:v1 .
-
-# python3
-cd python3_scripts && docker build -t microbiomeinformatics/${NAME}.python3:v1 .
 
 # GUNC
 cd GUNC && docker build -t microbiomeinformatics/${NAME}.gunc:v2 .
 
-# prokka
-cd prokka && docker build -t microbiomeinformatics/${NAME}.prokka:v1 .
+# GTDB-Tk
 
-# EggNOG
-cd eggnog && docker build -t microbiomeinformatics/${NAME}.eggnog-mapper:v1 .
+# IPS
 
 # mash2nwk
 cd mash2nwk && docker build -t microbiomeinformatics/${NAME}.mash2nwk:v1 .
+
+# mmseqs
+cd  mmseqs && docker build -t microbiomeinformatics/${NAME}.mmseqs:v1 .
+
+# panaroo
+cd panaroo && docker build -t microbiomeinformatics/${NAME}.panaroo:v1 .
+
+# prokka
+cd prokka && docker build -t microbiomeinformatics/${NAME}.prokka:v1 .
+
+# python3
+cd python3_scripts && docker build -t microbiomeinformatics/${NAME}.python3:v1 .
+
