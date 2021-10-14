d374027370f74e8b880e1f56c7f30fba7c4aaaeb
Ekaterina Sakharova <kates@ebi.ac.uk>
2021-10-14 09:29:37 +0100

    WIP

--- cluster/restructure/ftp_ebi.py
+++ cluster/restructure/ftp_ebi.py
@@ -34,20 +34,19 @@ uhgg_catalogue
                         ----- MGYG-HGUT-00001.faa
                         ----- MGYG-HGUT-00001.fna
                         ----- MGYG-HGUT-00001.fna.fai
-                        ----- MGYG-HGUT-00001.gff
+                        ----- MGYG-HGUT-00001.gff          ( make a file wit link)
                         ----- MGYG-HGUT-00001_InterProScan.tsv
                 ----- pan-genome
-                        ----- core_genes.faa               ( core_genes.txt )
-                        ----- genes_presence-absence.tsv   ( gene_presence_absence.Rtab )
+                        ----- core_genes.faa               ( ready - core_genes.txt )
+                        ----- genes_presence-absence.tsv   ( ready - gene_presence_absence.Rtab )
                         ----- mashtree.nwk                 ( ready )
-                        ----- pan-genome.faa ( ?? )
-                        ----- pan-genome.fna
+                        ----- pan-genome.fna               ( ready - initial fasta )
 
 uhgp_catalogue  (mmseqs)
-    --- uhgp-100.tar.gz
-    --- uhgp-50.tar.gz
-    --- uhgp-90.tar.gz
-    --- uhgp-95.tar.gz
+    --- mmseqs_1.0_outdir.tar.gz                           ( was uhgp-100.tar.gz)
+    --- mmseqs_0.5_outdir.tar.gz
+    --- mmseqs_0.9_outdir.tar.gz
+    --- mmseqs_0.95_outdir.tar.gz
 """
 
 # gffs from prokka -> gzip
