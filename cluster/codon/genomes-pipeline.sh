b5e4fe305142be129ecf786477b1a9b1af36d683
Ekaterina Sakharova <kates@ebi.ac.uk>
2021-10-27 11:25:57 +0100

    add debug

--- cluster/codon/genomes-pipeline.sh
+++ cluster/codon/genomes-pipeline.sh
@@ -22,12 +22,13 @@ MAIN_PATH="/nfs/production/rdf/metagenomics/pipelines/dev/genomes-pipeline/"
 CWL=${MAIN_PATH}/cwl/workflows/wf-main.cwl
 YML=${MAIN_PATH}/tests/cluster/wf-main_ena_small.yml
 
-while getopts :o:y:c:p: option; do
+while getopts :o:y:c:p:d: option; do
 	case "${option}" in
 		o) OUTDIR=${OPTARG};;
 		y) YML=${OPTARG};;
 		c) CWL=${OPTARG};;
 		p) MAIN_PATH=${OPTARG};;
+	    d) DEBUG=${OPTARG};;
 	esac
 done
 
@@ -37,5 +38,8 @@ export PATH=$PATH:${MAIN_PATH}/docker/genomes-catalog-update/scripts/
 chmod a+x ${MAIN_PATH}/docker/python3_scripts/*
 chmod a+x ${MAIN_PATH}/docker/genomes-catalog-update/scripts/*
 
-
-cwltool --singularity --preserve-entire-environment --leave-container --outdir ${OUTDIR} ${CWL} ${YML}
+if [ "${DEBUG}" == "True" ]; then
+    cwltool --singularity --preserve-entire-environment --debug --leave-container --outdir ${OUTDIR} ${CWL} ${YML}
+else
+    cwltool --singularity --preserve-entire-environment --leave-container --outdir ${OUTDIR} ${CWL} ${YML}
+fi
