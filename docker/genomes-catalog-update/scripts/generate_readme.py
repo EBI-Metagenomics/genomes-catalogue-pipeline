fc426834f72b27c4b2dfdb21060d3292e3ca3016
Tatiana Gurbich <tgurbich@gmail.com>
2021-12-13 16:39:11 +0000

    Removed extra tabs

--- docker/genomes-catalog-update/scripts/generate_readme.py
+++ docker/genomes-catalog-update/scripts/generate_readme.py
@@ -38,52 +38,52 @@ def process_metadata_table(metadata_table):
 def print_file(outfile_name, version, cat_url, num_genomes, num_species, ver_gtdb, ver_panaroo, ver_eggnog, ver_ips,
                study_list, biome):
     readme_text = '''
-    {version} release
-    ------------
-    
-    Website URL: {url}
-    
-    * A total of {num_genomes} prokaryotic genomes from the {biome} microbiome were clustered into {num_species} species representatives.
-    * Genomes from the following studies were used to generate the catalog: {study_list}
-    * Taxonomic annotations were generated with the Genome Taxonomy Database {ver_gtdb}. 
-    * Pan-genome analyses were performed with Panaroo {ver_panaroo} for all conspecific genomes.
-    * Functional annotation results generated with eggNOG (emapper-{ver_eggnog}) and InterProScan {ver_ips} are available for the genomes of all species representatives. COG and KEGG results were derived from the eggNOG annotations.
-    * A protein catalogue was produced with all protein coding sequences clustered at 100%, 95%, 90% and 50% amino acid identity.
-    
-    
-    ## The following files are available for download in each species directory within the species_catalogue/:
-    
-    - genome/
-        * [species_accession].faa : Protein sequence FASTA file of the species representative.
-        * [species_accession].fna : DNA sequence FASTA file of the genome assembly of the species representative.
-        * [species_accession].gff : Genome GFF file with various sequence annotations.
-        * [species_accession]_eggNOG.tsv : eggNOG annotations of the protein coding sequences.
-        * [species_accession]_InterProScan.tsv : InterProScan annotation of the protein coding sequences.
-        * [species_accession]_rRNAs.fasta : rRNA sequence FASTA file for the species representative.
-    
-    
-    ## For species where there is more than one conspecific genome, pan-genomes can be found in:
-           
-    - pan-genome/
-        * core_genes.txt : List of core genes for the pan-genome (genes found in >=90% of the genomes).
-        * pan-genome.fna : Nucleotide sequence FASTA file of the pan-genome.
-        * gene_presence_absence.Rtab : Presence/absence binary matrix of the pan-genome across all conspecific genomes.
-        * mashtree.nwk : Tree generated from the pairwise Mash distances of conspecific genomes.
-    
-    
-    ## Additional files available in the parent directory:
-    
-    - all_genomes/: Combined GFF/FASTA file (Prokka output) for each of the {num_genomes} genomes. 
-            
-    - genomes-all_metadata.tsv : Assembly statistics and metadata of all {num_genomes} genomes.
-    
-    - protein_catalogue/
-        * protein_catalogue-XX.tar.gz
-            - protein_catalogue-XX.faa : Protein FASTA file of the clustered, representative sequences.
-            - protein_catalogue-XX.tsv : Cluster membership of all the protein sequences.
-        For 90% identity catalog only:
-            - protein_catalogue-90_eggNOG.tsv : eggNOG annotation results of the protein catalogue.
-            - protein_catalogue-90_InterProScan.tsv : InterProScan annotation results of the protein catalogue.
+{version} release
+------------
+
+Website URL: {url}
+
+* A total of {num_genomes} prokaryotic genomes from the {biome} microbiome were clustered into {num_species} species representatives.
+* Genomes from the following studies were used to generate the catalog: {study_list}
+* Taxonomic annotations were generated with the Genome Taxonomy Database {ver_gtdb}. 
+* Pan-genome analyses were performed with Panaroo {ver_panaroo} for all conspecific genomes.
+* Functional annotation results generated with eggNOG (emapper-{ver_eggnog}) and InterProScan {ver_ips} are available for the genomes of all species representatives. COG and KEGG results were derived from the eggNOG annotations.
+* A protein catalogue was produced with all protein coding sequences clustered at 100%, 95%, 90% and 50% amino acid identity.
+
+
+## The following files are available for download in each species directory within the species_catalogue/:
+
+- genome/
+    * [species_accession].faa : Protein sequence FASTA file of the species representative.
+    * [species_accession].fna : DNA sequence FASTA file of the genome assembly of the species representative.
+    * [species_accession].gff : Genome GFF file with various sequence annotations.
+    * [species_accession]_eggNOG.tsv : eggNOG annotations of the protein coding sequences.
+    * [species_accession]_InterProScan.tsv : InterProScan annotation of the protein coding sequences.
+    * [species_accession]_rRNAs.fasta : rRNA sequence FASTA file for the species representative.
+
+
+## For species where there is more than one conspecific genome, pan-genomes can be found in:
+       
+- pan-genome/
+    * core_genes.txt : List of core genes for the pan-genome (genes found in >=90% of the genomes).
+    * pan-genome.fna : Nucleotide sequence FASTA file of the pan-genome.
+    * gene_presence_absence.Rtab : Presence/absence binary matrix of the pan-genome across all conspecific genomes.
+    * mashtree.nwk : Tree generated from the pairwise Mash distances of conspecific genomes.
+
+
+## Additional files available in the parent directory:
+
+- all_genomes/: Combined GFF/FASTA file (Prokka output) for each of the {num_genomes} genomes. 
+        
+- genomes-all_metadata.tsv : Assembly statistics and metadata of all {num_genomes} genomes.
+
+- protein_catalogue/
+    * protein_catalogue-XX.tar.gz
+        - protein_catalogue-XX.faa : Protein FASTA file of the clustered, representative sequences.
+        - protein_catalogue-XX.tsv : Cluster membership of all the protein sequences.
+    For 90% identity catalog only:
+        - protein_catalogue-90_eggNOG.tsv : eggNOG annotation results of the protein catalogue.
+        - protein_catalogue-90_InterProScan.tsv : InterProScan annotation results of the protein catalogue.
     '''.format(version=version, url=cat_url, num_genomes=num_genomes, num_species=num_species, ver_gtdb=ver_gtdb,
                ver_panaroo=ver_panaroo, ver_eggnog=ver_eggnog, ver_ips=ver_ips, study_list=study_list, biome=biome)
     with open(outfile_name, 'w') as outfile:
