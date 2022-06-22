a374a35f88768ef69546b71580af6477c94ff802
Ekaterina Sakharova <kates@ebi.ac.uk>
2022-06-22 16:10:22 +0100

    change download script

--- installation/download_db.sh
+++ installation/download_db.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+mkdir dbs && cd dbs
+
 # GUNC database
 wget ftp://ftp.ebi.ac.uk/pub/databases/metagenomics/genomes-pipeline/gunc_db_2.0.4.dmnd.gz && gunzip gunc_db_2.0.4.dmnd.gz
 
 # EGGnog database
 wget ftp://ftp.ebi.ac.uk/pub/databases/metagenomics/genomes-pipeline/eggnog_db.tgz && tar -xvzf eggnog_db.tgz
+mkdir -p eggnog/data && mv eggnog_proteins.dmnd eggnog.db eggnog/data
 
 # InterProScan database
+mkdir ips && cd ips
 wget ftp://ftp.ebi.ac.uk/pub/databases/metagenomics/genomes-pipeline/interproscan-5.52-86.0_data.tgz && tar -xvzf interproscan-5.52-86.0_data.tgz
-
+cd ..
 # GTDB-Tk Database
 wget https://data.gtdb.ecogenomic.org/releases/latest/auxillary_files/gtdbtk_data.tar.gz && tar -zxvf gtdbtk_data.tar.gz
 
