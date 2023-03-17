ed6faa34d7d4220ad45782023af6dea71152ebef
Tatiana Gurbich <tgurbich@gmail.com>
2023-03-17 13:02:39 +0000

    Made discarding strains optional

--- docker/genomes-catalog-update/scripts/parse_mash.py
+++ docker/genomes-catalog-update/scripts/parse_mash.py
@@ -18,16 +18,22 @@
 
 
 import argparse
+import logging
 import os
 from shutil import copy2
+
+logging.basicConfig(level=logging.INFO)
 
 NEW_SPECIES_CUTOFF = 0.05
 DISCARD_CUTOFF = 0.001
 
 
-def main(mash, genomes_file, outfolder, infolder):
+def main(mash, genomes_file, outfolder, infolder, discard_repeat_strains):
     scores = dict()
-    genomes = load_list(genomes_file)
+    if genomes_file:
+        genomes = load_list(genomes_file)
+    else:
+        genomes = os.listdir(infolder)
     with open(mash, 'r') as infile:
         for line in infile:
             if line == "\n":
@@ -41,8 +47,11 @@ def main(mash, genomes_file, outfolder, infolder):
                         scores[genome] = score
                 else:
                     scores[genome] = score
-    discard, new_strains, new_species = evaluate(genomes, scores)
-    generate_output(discard, new_strains, new_species, outfolder, infolder)
+    discard, new_strains, new_species = evaluate(genomes, scores, discard_repeat_strains)
+    if not discard_repeat_strains:
+        new_strains.update(discard)
+        discard = set()
+    generate_output(discard, new_strains, new_species, outfolder, infolder, discard_repeat_strains)
 
 
 def load_list(genomes_file):
@@ -53,7 +62,7 @@ def load_list(genomes_file):
     return genomes
 
 
-def evaluate(genomes, scores):
+def evaluate(genomes, scores, discard_repeat_strains):
     discard = set()
     new_strains = set()
     new_species = set()
@@ -66,19 +75,23 @@ def evaluate(genomes, scores):
             new_species.add(genome)
         else:
             new_strains.add(genome)
-    print('discard: {} new strain: {} new species: {}'.format(len(discard), len(new_strains), len(new_species)))
+    if discard_repeat_strains:
+        logging.info('discard: {} new strain: {} new species: {}'.format(len(discard), len(new_strains), len(new_species)))
+    else:
+        logging.info('new strain: {} new species: {}'.format(len(discard) + len(new_strains), len(new_species)))
     return discard, new_strains, new_species
 
 
-def generate_output(discard, new_strains, new_species, outfolder, infolder):
+def generate_output(discard, new_strains, new_species, outfolder, infolder, discard_repeat_strains):
     strains_folder = os.path.join(outfolder, 'New_strains')
     species_folder = os.path.join(outfolder, 'New_species')
     for f in [outfolder, strains_folder, species_folder]:
         if not os.path.exists(f):
             os.makedirs(f)
-    discard_file = os.path.join(outfolder, 'discarded_genomes.txt')
-    with open(discard_file, 'w') as discard_out:
-        discard_out.write('\n'.join(discard) + '\n')
+    if discard_repeat_strains:
+        discard_file = os.path.join(outfolder, 'discarded_genomes.txt')
+        with open(discard_file, 'w') as discard_out:
+            discard_out.write('\n'.join(discard) + '\n')
     for file_name in new_species:
         original_location = os.path.join(infolder, file_name)
         copy2(original_location, species_folder)
@@ -96,15 +109,19 @@ def parse_args():
     ''')
     parser.add_argument('-m', '--mash', required=True,
                         help='Path to the mash filename')
-    parser.add_argument('-e', '--evaluate-list', required=True,
+    parser.add_argument('-e', '--evaluate-list', required=False,
                         help='List of genomes to evaluate')
     parser.add_argument('-o', '--outfolder', required=True,
                         help='Path to folder where the results will be saved to')
     parser.add_argument('-f', '--input-folder', required=True,
                         help='Path to folder where the dereplicated new genome fasta files are located')
+    parser.add_argument('--discard-repeat-strains', action='store_true',
+                        help='If this flag is on, strains that are already present in the catalogue '
+                             'will be saved to the discarded_genomes.txt file and will not be included '
+                             'in the New_strains folder.')
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = parse_args()
-    main(args.mash, args.evaluate_list, args.outfolder, args.input_folder)
+    main(args.mash, args.evaluate_list, args.outfolder, args.input_folder, args.discard_repeat_strains)
