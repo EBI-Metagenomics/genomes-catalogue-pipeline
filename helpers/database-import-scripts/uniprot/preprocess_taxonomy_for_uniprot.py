22ab8e0f52986f7e94d41331c209782866b19df4
Tatiana Gurbich <tgurbich@c02d525wml86.windows.ebi.ac.uk>
2023-09-18 10:53:53 +0100

    WIP

--- helpers/database-import-scripts/uniprot/preprocess_taxonomy_for_uniprot.py
+++ helpers/database-import-scripts/uniprot/preprocess_taxonomy_for_uniprot.py
@@ -3,6 +3,7 @@
 import argparse
 import logging
 import os
+import subprocess
 import sys
 
 import gtdb_to_ncbi_majority_vote, gtdb_to_ncbi_majority_vote_v2
@@ -26,19 +27,44 @@ def main(gtdbtk_folder, outfile, taxonomy_version, taxonomy_release):
         tax = gtdb_to_ncbi_majority_vote_v2.Translate()
         
     tax_dict = tax.run(gtdbtk_folder, selected_archaea_metadata, selected_bacteria_metadata, "gtdbtk")
-    tax_dict_filtered = filter_tax(tax_dict)  # filtered out unknown species; key=MGYG, value=full NCBI lineage
-    ncbi_dump = load_ncbi(tax_ncbi)
+    #tax_dict_filtered = filter_tax(tax_dict)  # filtered out unknown species; key=MGYG, value=full NCBI lineage
+    #ncbi_dump = load_ncbi(tax_ncbi)
     # lookup tax id and print results to file
     with open(outfile, "w") as file_out:
-        for mgyg, lineage in tax_dict_filtered.items():
-            species = lineage.strip().split(";")[-1].replace("s__", "")
-            try:
-                file_out.write("{}\t{}\t{}\t{}\n".format(mgyg, species, lineage, ncbi_dump[species]))
-            except:
-                logging.error("Unable to obtain NCBI taxid for genome {}. Skipping genome.".format(mgyg))
-                sys.exit(1)
+        for mgyg, lineage in tax_dict.items():
+            #species = lineage.strip().split(";")[-1].replace("s__", "")
+            lowest_known_taxon = get_lowest_taxon(lineage)
+            if not lowest_known_taxon:
+                sys.exit("Genome {} doesn't have any known taxonomy".format(mgyg))
+            else:
+                run_taxonkit(lowest_known_taxon)
+            #try:
+            #    file_out.write("{}\t{}\t{}\t{}\n".format(mgyg, species, lineage, ncbi_dump[species]))
+            #except:
+            #    logging.error("Unable to obtain NCBI taxid for genome {}. Skipping genome.".format(mgyg))
+            #    sys.exit(1)
 
 
+def run_taxonkit(lowest_known_taxon):
+    command = ["/homes/tgurbich/Taxonkit/taxonkit", "name2taxid", "--data-dir", "/homes/tgurbich/Taxonkit/taxdump"]
+    try:
+        result = subprocess.run(command, input=lowest_known_taxon, text=True, stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE, check=True)
+        non_empty_line_count = len([line for line in result.stdout.split('\n') if line.strip()])
+        if non_empty_line_count > 1:
+            print("There multiple lines in output: {}".format(result.stdout))
+    except subprocess.CalledProcessError as e:
+        print("Error:", e.stderr)
+    
+    
+def get_lowest_taxon(lineage):
+    elements = lineage.strip().split(";")
+    for i in reversed(elements):
+        if not i.endswith("__"):
+            return i.split("__")[1]
+    return None
+    
+    
 def select_dump(taxonomy_release, db_dir):
     dump_dict = {
         "r202": "fullnamelineage_2020-11-01.dmp",
