a04116378fea4111ea4584194fb85bb62068623c
Tatiana Gurbich <tgurbich@gmail.com>
2023-10-19 16:41:15 +0100

    Added getting taxonomy from INSDC for known GCA accessions

--- helpers/database-import-scripts/uniprot/preprocess_taxonomy_for_uniprot.py
+++ helpers/database-import-scripts/uniprot/preprocess_taxonomy_for_uniprot.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 
 import argparse
+import json
 import logging
 import os
 import re
 import subprocess
 import sys
+
+import pandas as pd
+import requests
+from retry import retry
+import xmltodict
 
 import gtdb_to_ncbi_majority_vote, gtdb_to_ncbi_majority_vote_v2
 
@@ -15,12 +21,13 @@ TAXDUMP_PATH = "/nfs/production/rdf/metagenomics/pipelines/prod/assembly-pipelin
 DB_DIR = "/nfs/production/rdf/metagenomics/pipelines/prod/assembly-pipeline/taxonomy_dbs/"
 
 
-def main(gtdbtk_folder, outfile, taxonomy_version, taxonomy_release):
+def main(gtdbtk_folder, outfile, taxonomy_version, taxonomy_release, metadata_file):
     if not os.path.isdir(gtdbtk_folder):
         sys.exit("GTDB folder {} is not a directory. EXITING.".format(gtdbtk_folder))
     if not versions_compatible(taxonomy_version, taxonomy_release):
         sys.exit("GTDB versions {} and {} are not compatible".format(taxonomy_version, taxonomy_release))
     
+    gca_accessions, sample_accessions = parse_metadata(metadata_file)  # key = mgyg, value = gca accession
     
     selected_archaea_metadata, selected_bacteria_metadata = select_metadata(taxonomy_release, DB_DIR)
     tax_ncbi = select_dump(taxonomy_release, DB_DIR)
@@ -39,16 +46,128 @@ def main(gtdbtk_folder, outfile, taxonomy_version, taxonomy_release):
     # lowest_taxon_mgyg_dict: # key = mgyg, value = name of the lowest known taxon
     # lowest_taxon_lineage_dict: # key = lowest taxon, value = list of lineages where this taxon is lowest
     taxid_dict = run_taxonkit_on_dict(lowest_taxon_mgyg_dict, lineage_dict, lowest_taxon_lineage_dict)  
+    count_match = 0
+    count_mismatch = 0
     with open(outfile, "w") as file_out:
-        for key, value in lowest_taxon_mgyg_dict.items():
+        for key, lowest_taxon in lowest_taxon_mgyg_dict.items():
             lineage = lineage_dict[key]
             species_level = False if lineage.endswith("s__") else True
             taxid = taxid_dict[lowest_taxon_mgyg_dict[key]][lineage]
-            file_out.write("{}\t{}\t{}\t{}\t{}\n".format(
-                key, lowest_taxon_mgyg_dict[key], lineage, taxid, species_level))
+            print("before: ", lineage, lowest_taxon)
+            
+            if key in gca_accessions:
+                gca_accession = gca_accessions[key]
+            else:
+                logging.debug("Using sample to get GCA")
+                gca_accession = get_gca_accession(sample_accessions[key])
+                logging.debug("Got this: {}".format(gca_accession))
+            if not gca_accession == "N/A":
+                # this means there is already a taxid in INSD for this genome
+                insdc_taxid = lookup_taxid_online(gca_accession)
+                if not taxid == insdc_taxid:
+                    lineage, lowest_taxon, species_level = lookup_lineage(insdc_taxid)
+            file_out.write("{}\t{}\t{}\t{}\t{}\t{}\n".format(
+                key, lowest_taxon, lineage, gca_accession, insdc_taxid, species_level))
     logging.info("Printed results to {}".format(outfile))
+    # known_taxid = get_taxid(metadata['Genome']['GCA'])
 
 
+def lookup_lineage(insdc_taxid):
+    command = ["/homes/tgurbich/Taxonkit/taxonkit", "reformat", "--data-dir",
+               TAXDUMP_PATH, "-I", "1", "-P"]
+    result = subprocess.run(command, input=insdc_taxid, text=True, stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE, check=True)
+    try:
+        lineage = result.stdout.strip().split("\t")[1]
+        retrieved_name = re.sub("(;[a-z]__)+$", "", lineage).split(";")[-1]
+        retrieved_name = re.sub("[a-z]__", "", retrieved_name)
+        assert retrieved_name, "Could not get retrieved name for lineage".format(lineage)
+        print("Got INSDC lineage", lineage, retrieved_name)
+        return lineage, retrieved_name, False
+    except:
+        logging.error("Unable to retrieve lineage from taxid.")
+        sys.exit("Aborting.")
+    
+
+def parse_metadata(metadata_file):
+    gca_accessions = dict()
+    sample_accessions = dict()
+    original_df = pd.read_csv(metadata_file, sep='\t')
+    filtered_df = original_df[original_df['Genome'] == original_df['Species_rep']]
+    for index, row in filtered_df.iterrows():
+        sample_accessions[row['Genome']] = row['Sample_accession']
+        if row['Genome_accession'].startswith("GCA"):
+            gca_accessions[row['Genome']] = row['Genome_accession']
+            print("GCA accession already in metadata table {}".format(row['Genome_accession']))
+    return gca_accessions, sample_accessions
+
+
+def get_gca_accession(sample):
+    print(sample)
+    if not isinstance(sample, str):
+        return "N/A"
+    if sample == "NA" or sample.startswith("P"):
+        return "N/A"
+    api_endpoint = "https://www.ebi.ac.uk/ena/portal/api/filereport"
+    full_url = "{}?accession={}&result=assembly&fields=accession".format(api_endpoint, sample)
+    r = run_request(full_url)
+    if r.ok:
+        lines = r.text.split('\n')
+        if len(lines) > 3:
+            # check if the same GCA accession is duplicated; if there are distinct GCAs linked to the same 
+            # sample accession, don't take any of them because we don't know which one is correct
+            filtered_lines = list(set([item for item in lines if item != "" and item != "accession"]))
+            if len(filtered_lines) == 1:
+                gca_line = filtered_lines[0]
+            else:
+                gca_line = "N/A"
+        else:
+            try:
+                gca_line = next(line for line in lines if line.startswith("GCA"))
+            except:
+                gca_line = "N/A"
+        print(gca_line)
+        return gca_line
+    else:
+        logging.error("Failed to load data from ENA API when querying sample {}".format(sample))
+        sys.exit("Aborting.")
+    
+
+def lookup_taxid_online(gca_acc):
+    taxid = "N/A"
+    if not gca_acc == "N/A":
+        taxid = load_taxid_from_xml(gca_acc)
+    return taxid
+
+
+def load_taxid_from_xml(gca_acc):
+    xml_url = 'https://www.ebi.ac.uk/ena/browser/api/xml/{}'.format(gca_acc)
+    try:
+        r = run_xml_request(xml_url)
+    except:
+        logging.warning("Unable to get XML for accession {}. Recording as N/A.".format(gca_acc))
+        return "N/A"
+    if r.ok:
+        try:
+            data_dict = xmltodict.parse(r.content)
+            json_dump = json.dumps(data_dict)
+            json_data = json.loads(json_dump)
+        except:
+            logging.exception("Unable to load json from API for accession {}".format(gca_acc))
+            print(r.text)
+            sys.exit("Aborting.")
+    else:
+        logging.error('Could not retrieve xml for accession {}'.format(gca_acc))
+        logging.error(r.text)
+        sys.exit("Aborting.")
+    try:
+        taxid = json_data["ASSEMBLY_SET"]["ASSEMBLY"]["TAXON"]["TAXON_ID"]
+        return taxid
+    except:
+        logging.error("Couldn't print json data")
+        return "N/A"
+        
+    
 def load_synonyms():
     namesdump = os.path.join(TAXDUMP_PATH, "names.dmp")
     synonym_dict = dict()
@@ -71,13 +190,13 @@ def run_taxonkit_on_dict(lowest_taxon_mgyg_dict, lineage_dict, lowest_taxon_line
         result = subprocess.run(command, input=input_data, text=True, stdout=subprocess.PIPE,
                                 stderr=subprocess.PIPE, check=True)
         taxid_dict = process_taxonkit_output(result.stdout)
-        filtered_taxid_dict = filter_taxid_dict(taxid_dict, lineage_dict, lowest_taxon_lineage_dict)  # resolve cases where multiple taxid are assigned to taxon
+        filtered_taxid_dict = filter_taxid_dict(taxid_dict, lowest_taxon_lineage_dict)  # resolve cases where multiple taxid are assigned to taxon
         return filtered_taxid_dict
     except subprocess.CalledProcessError as e:
         print("Error:", e.stderr)
 
 
-def filter_taxid_dict(taxid_dict, lineage_dict, lowest_taxon_lineage_dict):
+def filter_taxid_dict(taxid_dict, lowest_taxon_lineage_dict):
     """
     Resolve cases where multiple taxids are assigned to the same taxon by matching domain and phylum. 
     If domain and phylum match multiple taxon ids, the function picks the first one.
@@ -109,14 +228,14 @@ def filter_taxid_dict(taxid_dict, lineage_dict, lowest_taxon_lineage_dict):
                                         stderr=subprocess.PIPE, check=True)
                 try:
                     lineage = result.stdout.strip().split("\t")[1]
-                    logging.debug("Checking dump lineage", lineage)
+                    logging.debug("Checking dump lineage".format(lineage))
                     retrieved_name = re.sub(";+$", "", lineage).split(";")[-1]
                     assert retrieved_name, "Could not get retrieved name for lineage".format(lineage)
-                    logging.debug("Checking name from dump lineage is", retrieved_name)
+                    logging.debug("Checking name from dump lineage is".format(retrieved_name))
                     expected_domains_and_phyla = get_domains_and_phyla(lowest_taxon_lineage_dict[taxon_name])
-                    logging.debug("expected phyla obtained", expected_domains_and_phyla)
+                    logging.debug("expected phyla obtained: {}".format(expected_domains_and_phyla))
                     retrieved_domain = lineage.split(";")[0]
-                    logging.debug("Checking domain", retrieved_domain)
+                    logging.debug("Checking domain {}".format(retrieved_domain))
                     if retrieved_domain in expected_domains_and_phyla:
                         logging.debug("Domain match")
                         for phylum in expected_domains_and_phyla[retrieved_domain]:
@@ -151,8 +270,8 @@ def filter_taxid_dict(taxid_dict, lineage_dict, lowest_taxon_lineage_dict):
                                     filtered_taxid_dict.setdefault(taxon_name, dict())
                                     if matching_lineage not in filtered_taxid_dict[taxon_name]:
                                         filtered_taxid_dict[taxon_name][matching_lineage] = taxid
-                    logging.debug(lineage, retrieved_name)
-                    logging.debug(lowest_taxon_lineage_dict[taxon_name])
+                    logging.debug("Lineage {} retrieved name {}".format(lineage, retrieved_name))
+                    logging.debug("Lowest taxon {}".format(lowest_taxon_lineage_dict[taxon_name]))
                     # first check that the name matches
                 except AssertionError as e:
                     logging.error("Assertion error: {}".format(e))
@@ -296,6 +415,20 @@ def versions_compatible(taxonomy_version, taxonomy_release):
     else:
         return True
 
+
+@retry(tries=5, delay=10, backoff=1.5)
+def run_request(full_url):
+    r = requests.get(url=full_url)
+    r.raise_for_status()
+    return r
+
+
+@retry(tries=5, delay=10, backoff=1.5)
+def run_xml_request(xml_url):
+    r = requests.get(xml_url)
+    r.raise_for_status()
+    return r
+
     
 def parse_args():
     parser = argparse.ArgumentParser(description="The script takes in a GTDB-Tk output folder and "
@@ -308,10 +441,12 @@ def parse_args():
                         help='Version of GTDB-Tk, "1" or "2". Default = "2".')
     parser.add_argument('-r', '--taxonomy-release', choices=['r202', 'r207', 'r214'], default="r214",
                         help='Version of GTDB-Tk, "r202", "r207" or "r214". Default = "r214".')
+    parser.add_argument('-m', '--metadata', required=True,
+                        help='Path to the metadata table.')
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = parse_args()
-    main(args.gtdbtk_folder, args.outfile, args.taxonomy_version, args.taxonomy_release)
+    main(args.gtdbtk_folder, args.outfile, args.taxonomy_version, args.taxonomy_release, args.metadata)
     
