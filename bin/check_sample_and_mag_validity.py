4968eae0969598f1964eed5517f26e189ff72a20
Tatiana Gurbich <tgurbich@gmail.com>
2025-01-24 13:54:12 +0000

    Added multithreading to the sample and mag validity checker

--- bin/check_sample_and_mag_validity.py
+++ bin/check_sample_and_mag_validity.py
@@ -1,26 +1,8 @@
-#!/usr/bin/env python3
-# coding=utf-8
-
-# This file is part of MGnify genomes catalogue pipeline.
-#
-# MGnify genome analysis pipeline is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# MGnify genome analysis pipeline is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with MGnify genome analysis pipeline. If not, see <https://www.gnu.org/licenses/>.
-
-
 import argparse
 import logging
 import os
 import sys
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 import requests
 from retry import retry
@@ -28,7 +10,7 @@ from retry import retry
 logging.basicConfig(level=logging.INFO)
 
 
-def main(input_folder, remove_list_file, outfile):
+def main(input_folder, remove_list_file, outfile, num_threads):
     if not outfile:
         outfile = "GENOME_CHECK_FAILED_ACCESSIONS"
     metadata_table_file = os.path.join(input_folder, "ftp", "genomes-all_metadata.tsv")
@@ -40,60 +22,92 @@ def main(input_folder, remove_list_file, outfile):
     else:
         remove_list = list()
     sample_mag_dictionary = load_metadata_table(metadata_table_file, remove_list)
-    missing_samples = list()
-    missing_genomes = list()
-    for sample, genome_list in sample_mag_dictionary.items():
-        if sample == 'NA':
-            continue
-        keep_genomes = True
-        try:
-            r = run_full_url_request("https://www.ebi.ac.uk/ena/browser/api/xml/{}".format(sample))
-        except:
-            logging.info("Sample {} not found in ENA. Error {}".format(sample, r.status_code))
-            missing_samples.append(sample)
-            keep_genomes = False
-        for genome in genome_list:
-            if keep_genomes:
-                if genome.startswith("GUT_"):
-                    continue
-                else:
-                    if genome.startswith("GCA_"):
-                        endpoint = "xml"
-                    else:
-                        endpoint = "text"
-                    try:
-                        r = run_full_url_request("https://www.ebi.ac.uk/ena/browser/api/{}/{}".format(
-                            endpoint, genome))
-                    except:
-                        logging.info("Genome {} not found in ENA. Error {}".format(genome, r.status_code))
-                        missing_genomes.append(genome)
-            else:
-                missing_genomes.append(genome)
+
+    missing_samples = []
+    missing_genomes = []
+
+    # Multithreading for processing samples
+    with ThreadPoolExecutor(max_workers=num_threads) as sample_executor:
+        sample_futures = {
+            sample_executor.submit(process_sample, sample, genome_list, num_threads): sample
+            for sample, genome_list in sample_mag_dictionary.items() if sample != 'NA'
+        }
+        for future in as_completed(sample_futures):
+            try:
+                sample_result = future.result()
+                missing_samples.extend(sample_result['missing_samples'])
+                missing_genomes.extend(sample_result['missing_genomes'])
+            except Exception as e:
+                logging.error("Error processing sample: {}".format(e))
+
     if len(missing_samples) > 0 or len(missing_genomes) > 0:
         logging.info("Found genomes and/or samples from the previous catalogue version that are no longer in ENA")
         with open(outfile, "w") as file_out:
             for sample in missing_samples:
-                file_out.write("{}\n".format(sample))
+                file_out.write("{}\tsample\n".format(sample))
             for genome in missing_genomes:
-                file_out.write("{}\n".format(genome))
-        # The user should evaluate the missing genomes and either 
-        # resolve the issue if the missing status is incorrect or add them to the list of genomes to be 
-        # removed from the catalogue
+                file_out.write("{}\tgenome\n".format(genome))
         logging.error("Missing genomes and samples are saved to {}. Check that these are indeed missing in ENA, "
                       "add them to the file containing a list of genomes to remove and restart the pipeline.".
                       format(outfile))
     else:
         logging.info("No missing genomes or samples found in the previous version of the catalogue")
-        # create an empty file
         with open("GENOME_CHECK_ALL_GENOMES_OK", "w") as file_out:
             pass
-    
-    
+
+
 @retry(tries=3, delay=10, backoff=1.5)
 def run_full_url_request(full_url):
     r = requests.get(url=full_url)
     r.raise_for_status()
     return r
+
+
+def process_sample(sample, genome_list, num_threads):
+    """
+    Process a single sample and its genomes.
+    Returns a dictionary with missing samples and genomes.
+    """
+    missing_samples = []
+    missing_genomes = []
+
+    # Check if the sample exists
+    try:
+        run_full_url_request(f"https://www.ebi.ac.uk/ena/browser/api/xml/{sample}")
+        keep_genomes = True
+    except requests.exceptions.RequestException as e:
+        logging.info("Sample {} not found in ENA. Error: {}".format(sample, e))
+        missing_samples.append(sample)
+        keep_genomes = False
+
+    # Multithreading for processing genomes within the sample
+    if keep_genomes:
+        with ThreadPoolExecutor(max_workers=num_threads) as genome_executor:
+            genome_futures = {
+                genome_executor.submit(fetch_genome_data, genome): genome
+                for genome in genome_list if not genome.startswith("GUT_")
+            }
+            for future in as_completed(genome_futures):
+                genome = genome_futures[future]
+                try:
+                    future.result()
+                except Exception as e:
+                    logging.info("Genome {} not found in ENA. Error: {}".format(genome, e))
+                    missing_genomes.append(genome)
+    else:
+        missing_genomes.extend(genome_list)
+
+    return {'missing_samples': missing_samples, 'missing_genomes': missing_genomes}
+
+
+def fetch_genome_data(genome):
+    """Fetch genome data with the appropriate API endpoint."""
+    if genome.startswith("GCA_"):
+        endpoint = "xml"
+    else:
+        endpoint = "text"
+    url = f"https://www.ebi.ac.uk/ena/browser/api/{endpoint}/{genome}"
+    run_full_url_request(url)
 
 
 def load_remove_list(remove_list_file, metadata_table_file):
@@ -135,7 +149,7 @@ def load_translation(metadata_table_file):
             insdc_acc = parts[insdc_index]
             translation_dict[mgyg] = insdc_acc
     return translation_dict
-    
+
 
 def load_metadata_table(metadata_table_file, remove_list):
     sample_mag_dictionary = dict()
@@ -165,8 +179,8 @@ def load_metadata_table(metadata_table_file, remove_list):
                                             "{}. Check that the list of genomes to remove is correct".
                                             format(metadata_table_file))
     return sample_mag_dictionary
-    
-    
+
+
 def parse_args():
     parser = argparse.ArgumentParser(
         description=(
@@ -203,6 +217,16 @@ def parse_args():
             "specified, output will be saved to GENOME_CHECK_FAILED_ACCESSIONS"
         ),
     )
+    parser.add_argument(
+        "-t",
+        "--threads",
+        required=False,
+        default=16,
+        type=int,
+        help=(
+            "Number of threads to use. Default: 16"
+        ),
+    )
     return parser.parse_args()
 
 
@@ -212,4 +236,5 @@ if __name__ == "__main__":
         args.input_folder,
         args.remove_list,
         args.outfile,
+        args.threads,
     )
