337b6ba0b459071fc7d2cff326d94f63dd014c5a
Tatiana Gurbich <tgurbich@gmail.com>
2025-03-24 09:48:26 +0000

    Separated rRNA handling betweek the euk and non-euk annotate_gff scripts

--- bin/annotate_euk_gff.py
+++ bin/annotate_euk_gff.py
@@ -509,7 +509,7 @@ def get_ncrnas(ncrnas_file):
 
 def prepare_rna_gff_fields(cols):
     rna_feature_name = "ncRNA"
-    if cols[1] in ["LSU_rRNA_bacteria", "SSU_rRNA_bacteria", "5S_rRNA"]:
+    if cols[1] in ["SSU_rRNA_eukarya", "LSU_rRNA_eukarya", "5_8S_rRNA", "5S_rRNA"]:
         rna_feature_name = "rRNA"
     # ncRNA classes are described here: https://www.insdc.org/submitting-standards/ncrna-vocabulary/
     ncrna_class = ""
