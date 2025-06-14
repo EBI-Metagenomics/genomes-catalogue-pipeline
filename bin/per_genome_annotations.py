40c348d315591af6aa042aa6dfd0f1926b66d931
Tatiana Gurbich <tgurbich@gmail.com>
2025-06-14 20:46:32 +0100

    Added a low memory workflow to the per genome annotations script

--- bin/per_genome_annotations.py
+++ bin/per_genome_annotations.py
@@ -27,22 +27,34 @@ logging.basicConfig(level=logging.INFO)
 def main(ips, eggnog, rep_list, outdir, mmseqs_tsv, cores):
     if not os.path.exists(outdir):
         os.makedirs(outdir)
-    # load representative accessions
-    with open(rep_list, "r") as rep_in:
-        genome_list = [line.strip().split(".")[0] for line in rep_in]
-    logging.info("Loaded the representative accessions.")
-    # load cluster information
-    pool = mp.Pool(cores)
+
+    with open(rep_list, "r") as f:
+        genome_list = {line.strip().split(".")[0] for line in f}
+    logging.info(f"Loaded {len(genome_list)} representative accessions.")
+
+    if len(genome_list) <= 10000:
+        logging.info("Using fast (memory-intensive) workflow.")
+        fast_workflow(ips, eggnog, genome_list, outdir, mmseqs_tsv, cores)
+    else:
+        logging.info("Using memory-efficient (slower) workflow.")
+        memory_efficient_workflow(ips, eggnog, genome_list, outdir, mmseqs_tsv, cores)
+
+
+# ------------------- FAST WORKFLOW -------------------
+
+def fast_workflow(ips, eggnog, genome_list, outdir, mmseqs_tsv, cores):
     global clusters
     clusters = dict()
+    pool = mp.Pool(cores)
     for chunk_start, chunk_size in make_tsv_chunks(mmseqs_tsv):
         pool.apply_async(
-            process_chunk,
+            process_chunk_fast,
             (chunk_start, chunk_size, genome_list, mmseqs_tsv),
             callback=append_values,
         )
     pool.close()
     pool.join()
+
     logging.info("Loaded cluster information.")
     # separate annotations by genome, load into dictionaries and generate result files
     header_ips, results_ips = load_annotations(ips, clusters)
@@ -54,22 +66,8 @@ def main(ips, eggnog, rep_list, outdir, mmseqs_tsv, cores):
     logging.info("Generated eggNOG output.")
 
 
-def make_tsv_chunks(file, size=1024 * 1024):
-    file_end = os.path.getsize(file)
-    with open(file, "rb") as f:
-        chunk_end = f.tell()
-        while True:
-            chunk_start = chunk_end
-            f.seek(size, 1)
-            f.readline()
-            chunk_end = f.tell()
-            yield chunk_start, chunk_end - chunk_start
-            if chunk_end > file_end:
-                break
-
-
-def process_chunk(chunk_start, chunk_size, genome_list, tsv_file):
-    result = list()
+def process_chunk_fast(chunk_start, chunk_size, genome_list, tsv_file):
+    result = []
     with open(tsv_file) as f:
         f.seek(chunk_start)
         lines = f.read(chunk_size).splitlines()
@@ -82,17 +80,8 @@ def process_chunk(chunk_start, chunk_size, genome_list, tsv_file):
 
 def append_values(returned_values):
     if returned_values:
-        for pair in returned_values:
-            clusters.setdefault(pair[0], set()).add(pair[1])
-
-
-def print_results(result_dict, header, outdir, tool):
-    for acc in result_dict.keys():
-        out_path = os.path.join(outdir, "{}_{}.tsv".format(acc, tool))
-        with open(out_path, "w") as file_out:
-            if header:
-                file_out.write(header + "\n")
-            file_out.write("\n".join(result_dict[acc]))
+        for rep, member in returned_values:
+            clusters.setdefault(rep, set()).add(member)
 
 
 def load_annotations(ann_file, clusters):
@@ -105,54 +94,136 @@ def load_annotations(ann_file, clusters):
                 header = line
             else:
                 rep_protein = line.split("\t")[0]
-                if rep_protein in clusters.keys():
-                    for member_protein in clusters[rep_protein]:
-                        genome = member_protein.split("_")[0]
-                        ann_result.setdefault(genome, list()).append(
-                            line.replace(rep_protein, member_protein)
-                        )
+                if rep_protein in clusters:
+                    for member in clusters[rep_protein]:
+                        genome = member.split("_")[0]
+                        ann_result.setdefault(genome, []).append(line.replace(rep_protein, member))
     return header, ann_result
 
 
+def print_results(result_dict, header, outdir, tool):
+    for genome, lines in result_dict.items():
+        out_path = os.path.join(outdir, f"{genome}_{tool}.tsv")
+        with open(out_path, "w") as f_out:
+            if header:
+                f_out.write(header + "\n")
+            f_out.write("\n".join(lines))
+
+
+# ------------------- MEMORY EFFICIENT WORKFLOW -------------------
+
+def memory_efficient_workflow(ips, eggnog, genome_list, outdir, mmseqs_tsv, cores):
+    cluster_map_path = "cluster_map.tsv"
+    process_mmseqs_to_file(mmseqs_tsv, genome_list, cluster_map_path, cores)
+    logging.info("Cluster information written to disk.")
+    stream_annotations(ips, cluster_map_path, outdir, "InterProScan")
+    logging.info("Processed InterProScan annotations.")
+    stream_annotations(eggnog, cluster_map_path, outdir, "eggNOG")
+    logging.info("Processed eggNOG annotations.")
+
+
+def process_mmseqs_to_file(tsv_file, genome_list, out_path, cores):
+    queue = mp.Queue(maxsize=cores * 4)
+    writer = mp.Process(target=write_cluster_map, args=(queue, out_path))
+    writer.start()
+
+    with mp.Pool(cores) as pool:
+        for chunk_start, chunk_size in make_tsv_chunks(tsv_file):
+            pool.apply_async(
+                process_chunk_memory_efficient,
+                args=(chunk_start, chunk_size, genome_list, tsv_file),
+                callback=lambda result: queue.put(result)
+            )
+        pool.close()
+        pool.join()
+
+    queue.put(None)
+    writer.join()
+
+
+def process_chunk_memory_efficient(chunk_start, chunk_size, genome_list, tsv_file):
+    result = []
+    with open(tsv_file, "r") as f:
+        f.seek(chunk_start)
+        lines = f.read(chunk_size).splitlines()
+        for line in lines:
+            if "\t" not in line:
+                continue
+            rep, member = line.strip().split("\t")
+            if member.split("_")[0] in genome_list:
+                result.append(f"{rep}\t{member}")
+    return result
+
+
+def write_cluster_map(queue, out_path):
+    with open(out_path, "w") as f:
+        while True:
+            item = queue.get()
+            if item is None:
+                break
+            for line in item:
+                f.write(line + "\n")
+
+
+def stream_annotations(ann_file, cluster_map_path, outdir, tool):
+    clusters = {}
+    with open(cluster_map_path, "r") as f:
+        for line in f:
+            rep, member = line.strip().split("\t")
+            clusters.setdefault(rep, []).append(member)
+
+    header = None
+    writers = set()
+
+    with open(ann_file, "r") as f:
+        for line in f:
+            line = line.strip()
+            if line.startswith("#query"):
+                header = line
+                continue
+            rep_protein = line.split("\t")[0]
+            if rep_protein not in clusters:
+                continue
+            for member_protein in clusters[rep_protein]:
+                genome = member_protein.split("_")[0]
+                replaced_line = line.replace(rep_protein, member_protein)
+                out_file = os.path.join(outdir, f"{genome}_{tool}.tsv")
+                mode = "a" if genome in writers else "w"
+                with open(out_file, mode) as f_out:
+                    if mode == "w" and header:
+                        f_out.write(header + "\n")
+                    f_out.write(replaced_line + "\n")
+                writers.add(genome)
+
+
+# ------------------- COMMON -------------------
+
+def make_tsv_chunks(file, size=10 * 1024 * 1024):
+    file_end = os.path.getsize(file)
+    with open(file, "rb") as f:
+        chunk_end = f.tell()
+        while True:
+            chunk_start = chunk_end
+            f.seek(size, 1)
+            f.readline()
+            chunk_end = f.tell()
+            yield chunk_start, chunk_end - chunk_start
+            if chunk_end >= file_end:
+                break
+
+
 def parse_args():
-    parser = argparse.ArgumentParser(
-        description=(
-            "Takes interproscan and eggNOG results for an MMseqs catalog as well "
-            "as a list of representative genomes and locations where the results "
-            "should be stored and creates individual annotation files for each "
-            "representative genome"
-        )
-    )
-    parser.add_argument(
-        "-i", "--ips", required=True, help="Path to the interproscan input file"
-    )
-    parser.add_argument(
-        "-e", "--eggnog", required=True, help="Path to the eggNOG input file"
-    )
-    parser.add_argument(
-        "-r",
-        "--rep-list",
-        required=True,
-        help="Path to a file containing species representatives",
-    )
-    parser.add_argument(
-        "-t",
-        "--mmseqs-tsv",
-        required=True,
-        help=(
-            "Path to the tsv file generated by MMseqs that contains information on"
-            " protein clusters"
-        ),
-    )
-    parser.add_argument(
-        "-o",
-        "--outdir",
-        required=True,
-        help="Path to the folder where the results will be stored",
-    )
-    parser.add_argument(
-        "-c", "--cores", required=True, type=int, help="Number of cores to use"
-    )
+    parser = argparse.ArgumentParser(description="Takes interproscan and eggNOG results for an "
+                                                 "MMseqs catalog as well as a list of representative "
+                                                 "genomes and locations where the results should be "
+                                                 "stored and creates individual annotation files for "
+                                                 "each representative genome")
+    parser.add_argument("-i", "--ips", required=True, help="Path to InterProScan input")
+    parser.add_argument("-e", "--eggnog", required=True, help="Path to eggNOG input")
+    parser.add_argument("-r", "--rep-list", required=True, help="Representative genomes list")
+    parser.add_argument("-t", "--mmseqs-tsv", required=True, help="MMseqs TSV file")
+    parser.add_argument("-o", "--outdir", required=True, help="Output directory")
+    parser.add_argument("-c", "--cores", required=True, type=int, help="Number of CPU cores")
     return parser.parse_args()
 
 
