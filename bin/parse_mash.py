03fc3604fea9c2f28e9f0c10c5748ad5c7f26680
Tatiana Gurbich <tgurbich@gmail.com>
2025-11-17 15:36:17 +0000

    Refactored parse_mash.py

--- bin/parse_mash.py
+++ bin/parse_mash.py
@@ -25,79 +25,96 @@ from shutil import copy2
 logging.basicConfig(level=logging.INFO)
 
 NEW_SPECIES_CUTOFF = 0.05
-DISCARD_CUTOFF = 0.001
+SAME_STRAIN_CUTOFF = 0.001
 
 
-def main(mash, genomes_file, outfolder, infolder, discard_repeat_strains):
+def main(mash, genomes_file, outfolder, infolder, remove_file):
     scores = dict()
+    # We need to use a list of genomes to check because not all query genomes will be in the mash file (everything
+    # that didn't have a close enough hit is filtered out)
     if genomes_file:
         genomes = load_list(genomes_file)
     else:
         genomes = os.listdir(infolder)
+    if remove_file:
+        remove_list = load_list(remove_file, remove_ext=True)
+    else:
+        remove_list = []
     with open(mash, 'r') as infile:
+        # mash output files have no heading and lines look like this:
+        # MGYG000518640.fna       renamed_genomes/MGYG000535623.fa        0.0201478       0       487/1000
         for line in infile:
             if line == "\n":
                 break
-            genome_path = line.strip().split()[1]
-            genome = genome_path.split('/')[-1]
-            if genome in genomes:
-                score = float(line.strip().split()[2])
-                if genome in scores:
-                    if score < scores[genome]:
-                        scores[genome] = score
-                else:
-                    scores[genome] = score
-    discard, new_strains, new_species = evaluate(genomes, scores, discard_repeat_strains)
-    if not discard_repeat_strains:
-        new_strains.update(discard)
-        discard = set()
-    generate_output(discard, new_strains, new_species, outfolder, infolder, discard_repeat_strains)
+            catalogue_genome, query_genome_path, score, _, _ = line.strip().split()
+            query_genome = os.path.basename(query_genome_path)  # get just the genome file name
+            if query_genome in genomes and os.path.splitext(query_genome)[0] not in remove_list:
+                score = float(score)
+                # Update only if new score is lower (or missing)
+                previous_score = scores.get(query_genome)
+                if previous_score is None or score < previous_score:
+                    scores[query_genome] = score
+    same_strains, new_strains, new_species = evaluate(genomes, scores)
+    generate_output(same_strains, new_strains, new_species, outfolder, infolder)
 
 
-def load_list(genomes_file):
+def load_list(genomes_file, remove_ext=False):
     genomes = set()
     with open(genomes_file, 'r') as infile:
         for line in infile:
-            genomes.add(line.strip())
+            fasta_file = os.path.basename(line.strip())
+            if remove_ext:
+                fasta_file = os.path.splitext(fasta_file)[0]
+            genomes.add(fasta_file)
     return genomes
 
 
-def evaluate(genomes, scores, discard_repeat_strains):
-    discard = set()
+def copy_file_list(names, infolder, outfolder):
+    """Copies each file in `names` from `infolder` to `outfolder`."""
+    for name in names:
+        src = os.path.join(infolder, name)
+        dst = os.path.join(outfolder, name)
+        copy2(src, dst)
+
+
+def evaluate(genomes, scores):
+    same_strains = set()
     new_strains = set()
     new_species = set()
     for genome in genomes:
         if genome not in scores:
             new_species.add(genome)
-        elif scores[genome] < DISCARD_CUTOFF:
-            discard.add(genome)
+        elif scores[genome] < SAME_STRAIN_CUTOFF:
+            same_strains.add(genome)
         elif scores[genome] > NEW_SPECIES_CUTOFF:
             new_species.add(genome)
         else:
             new_strains.add(genome)
-    if discard_repeat_strains:
-        logging.info('discard: {} new strain: {} new species: {}'.format(len(discard), len(new_strains), len(new_species)))
-    else:
-        logging.info('new strain: {} new species: {}'.format(len(discard) + len(new_strains), len(new_species)))
-    return discard, new_strains, new_species
+
+    logging.info('same strain: {} new strain: {} new species: {}'.format(len(same_strains), len(new_strains), 
+                                                                         len(new_species)))
+    return same_strains, new_strains, new_species
 
 
-def generate_output(discard, new_strains, new_species, outfolder, infolder, discard_repeat_strains):
-    strains_folder = os.path.join(outfolder, 'New_strains')
-    species_folder = os.path.join(outfolder, 'New_species')
-    for f in [outfolder, strains_folder, species_folder]:
-        if not os.path.exists(f):
-            os.makedirs(f)
-    if discard_repeat_strains:
-        discard_file = os.path.join(outfolder, 'discarded_genomes.txt')
-        with open(discard_file, 'w') as discard_out:
-            discard_out.write('\n'.join(discard) + '\n')
-    for file_name in new_species:
-        original_location = os.path.join(infolder, file_name)
-        copy2(original_location, species_folder)
-    for file_name in new_strains:
-        original_location = os.path.join(infolder, file_name)
-        copy2(original_location, strains_folder)
+def generate_output(repeat_strains, new_strains, new_species, outfolder, infolder):
+    # Output paths
+    new_species_folder = os.path.join(outfolder, 'New_species')
+    new_strains_file = os.path.join(outfolder, 'new_strains.txt')
+    repeat_strains_file = os.path.join(outfolder, 'repeat_strains.txt')
+    
+    # Create output root and new species folder
+    os.makedirs(outfolder, exist_ok=True)
+    os.makedirs(new_species_folder, exist_ok=True)
+    
+    # ---- Write strain lists to text files ----
+    with open(new_strains_file, 'w') as out_ns:
+        out_ns.write("\n".join(new_strains) + "\n" if new_strains else "")
+
+    with open(repeat_strains_file, 'w') as out_rs:
+        out_rs.write("\n".join(repeat_strains) + "\n" if repeat_strains else "")
+
+    # ---- Copy only new species ----
+    copy_file_list(new_species, infolder, new_species_folder)
 
 
 def parse_args():
@@ -110,18 +127,17 @@ def parse_args():
     parser.add_argument('-m', '--mash', required=True,
                         help='Path to the mash filename')
     parser.add_argument('-e', '--evaluate-list', required=False,
-                        help='List of genomes to evaluate')
+                        help='List of genomes to evaluate. Include extensions (same as mash query genomes)')
     parser.add_argument('-o', '--outfolder', required=True,
                         help='Path to folder where the results will be saved to')
     parser.add_argument('-f', '--input-folder', required=True,
                         help='Path to folder where the dereplicated new genome fasta files are located')
-    parser.add_argument('--discard-repeat-strains', action='store_true',
-                        help='If this flag is on, strains that are already present in the catalogue '
-                             'will be saved to the discarded_genomes.txt file and will not be included '
-                             'in the New_strains folder.')
+    parser.add_argument('-f', '--remove-list', required=False,
+                        help='Path to file containing a list of accessions from the previous catalogue '
+                             'version that are being removed during the update.')
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = parse_args()
-    main(args.mash, args.evaluate_list, args.outfolder, args.input_folder, args.discard_repeat_strains)
+    main(args.mash, args.evaluate_list, args.outfolder, args.input_folder, args.remove_list)
