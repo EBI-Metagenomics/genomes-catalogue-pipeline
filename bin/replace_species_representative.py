3a2c116b0bfe4e074abbdc3776684ecfac58948f
Tatiana Gurbich <tgurbich@gmail.com>
2026-03-26 11:48:04 +0000

    Modified the handling of output file names

--- bin/replace_species_representative.py
+++ bin/replace_species_representative.py
@@ -52,6 +52,8 @@ class Quality:
     
 def main(cluster_split_file, output_prefix, assembly_stats_file, isolates_file, checkm_file, remove_list_file, 
          new_species_split_file=None, new_strain_file=None, repeat_strain_file=None):
+    report_output_file = f"{output_prefix}_cluster_rep_changes_report.tsv"
+    clusters_output_file = f"{output_prefix}_clusters_split.txt"
     new_strain_placement = (
         load_strain_placement(new_strain_file, same_strain=False)  # strain_acc → Placement
         if new_strain_file else {}
@@ -70,7 +72,7 @@ def main(cluster_split_file, output_prefix, assembly_stats_file, isolates_file, 
     # everything - this is not an update, just a reannotation
     if not (new_strain_placement or remove_list or repeat_strain_placement or new_species_split_file):
         logging.info("No genomes are added or removed, printing old catalogue results and existing.")
-        output_existing_drep_tables(cluster_split_file, output_prefix)
+        output_existing_drep_tables(cluster_split_file, clusters_output_file, report_output_file)
         return
     
     logging.info("Evaluating changes...")
@@ -87,12 +89,11 @@ def main(cluster_split_file, output_prefix, assembly_stats_file, isolates_file, 
                                                                               remove_list)
 
     sanity_check(replacement_results, remove_list, current_clusters, new_strain_placement, stats_to_print)
-    write_report_tsv(report_to_print, f"{output_prefix}_cluster_rep_changes_report.tsv")
-    write_cluster_split_file(replacement_results, output_prefix, new_species_split_file)
+    write_report_tsv(report_to_print, report_output_file)
+    write_cluster_split_file(replacement_results, clusters_output_file, new_species_split_file)
 
 
-def write_cluster_split_file(replacement_results, output_prefix, new_species_split_file):
-    output_file = f"{output_prefix}_clusters_split.txt"
+def write_cluster_split_file(replacement_results, output_file, new_species_split_file):
     counter = 0
     with open(output_file, "w") as f_out:
         # If there's an existing split file, copy it and get the last cluster number
@@ -423,9 +424,10 @@ def remove_genomes_from_clusters(current_clusters, remove_list):
     return current_clusters_minus_removed, remove_log
     
     
-def output_existing_drep_tables(cluster_split_file, output_prefix):
-    updated_cluster_split_file = f"{output_prefix}_clusters_split.txt"
-    shutil.copy(cluster_split_file, updated_cluster_split_file)
+def output_existing_drep_tables(cluster_split_file, clusters_output_file, report_output_file):
+    shutil.copy(cluster_split_file, clusters_output_file)
+    with open(report_output_file, "w") as report_out:
+        report_out.write("No genomes removed or added, no changes to the clusters were made.")
     logging.info("No changes made to the clusters. Original file contents are written to output.")
 
 
