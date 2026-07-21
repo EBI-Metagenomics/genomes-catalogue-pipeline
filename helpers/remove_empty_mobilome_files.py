2926cb9a629c925386d30780b9395ac6ffde4c32
Alejandra Escobar <ales@ebi.ac.uk>
2026-07-21 10:30:10 +0100

    Update mobilome file handling for gzip support

--- helpers/remove_empty_mobilome_files.py
+++ helpers/remove_empty_mobilome_files.py
@@ -24,11 +24,11 @@ import re
 def main(results_folder):
     count_total = 0
     count_removed = 0
-    pattern = os.path.join(results_folder, "species_catalogue", '**', '*_mobilome.gff')
+    pattern = os.path.join(results_folder, "species_catalogue", '**', '*_mobilome.gff.gz')
     all_matching_files = glob.glob(pattern, recursive=True)
     # Matching files will include mobilome-only files (MGYG123_mobilome.gff) and full annotation with mobilome
     # (MGYG123_annotated_with_mobilome.gff). Exclude the latter - filtered files should only have mobilome-only GFFs.
-    mobilome_regex = re.compile(r'[^/\\]+(?<!_annotated_with)_mobilome\.gff$')
+    mobilome_regex = re.compile(r'[^/\\]+(?<!_annotated_with)_mobilome\.gff.gz$')
     filtered_files = [file for file in all_matching_files if mobilome_regex.search(os.path.basename(file))]
     for mobilome_file in filtered_files:
         count_total += 1
@@ -36,13 +36,24 @@ def main(results_folder):
         if not file_has_contents:
             count_removed += 1
             print("Cleaning {}".format(mobilome_file))
-            with open(mobilome_file, "w") as f_out:
+            with gzip.open(mobilome_file, "wt") as f_out:
                 f_out.write("##gff-version 3\n")
     print("Removed {} empty mobilome files out of {} total mobilome files".format(count_removed, count_total))
         
-        
+
+def open_file(filename):
+    """
+    Open a file, handling both compressed (.gz) and uncompressed files.
+    Returns a file handle that can be used for reading.
+    """
+    if filename.endswith('.gz'):
+        return gzip.open(filename, 'rt')  # 'rt' for text mode
+    else:
+        return open(filename, 'r')
+
+
 def evaluate_file(mobilome_file):
-    with open(mobilome_file, 'r') as f:
+    with open_file(mobilome_file) as f:                
         for line in f:
             if line.startswith("##FASTA"):
                 return False
